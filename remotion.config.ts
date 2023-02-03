0ccb797dd99d3a723a446319504a09210e8e8219
Jonny Burger <jonathanburger11@gmail.com>
2023-02-03 10:58:36 +0100

    new config format style

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,7 +1,7 @@
 import {Config} from 'remotion';
 import {webpackOverride} from './src/webpack-override';
 
-Config.Rendering.setImageFormat('jpeg');
-Config.Output.setOverwriteOutput(true);
+Config.setImageFormat('jpeg');
+Config.setOverwriteOutput(true);
 
-Config.Bundling.overrideWebpackConfig(webpackOverride);
+Config.overrideWebpackConfig(webpackOverride);
