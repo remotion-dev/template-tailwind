1e6ce4ea456d8e35de92a49414bc0fd909b74333
Patric Salvisberg <patric.salvisberg@gmail.com>
2023-06-09 13:35:20 +0200

    config update

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,3 +1,8 @@
+// See all configuration options: https://remotion.dev/docs/config
+// Each option also is available as a CLI flag: https://remotion.dev/docs/cli
+
+// Note: The configuration file does not apply if you use the Node.js API!
+
 import {Config} from '@remotion/cli/config';
 import {webpackOverride} from './src/webpack-override';
 
